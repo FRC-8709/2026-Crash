2c0d7409737b0e5858ba33fc611b2f78900d20da
Louis <louisrussell1030@gmail.com>
2026-02-13 13:54:10 -0500

    testing possible other print to terminal
    May or may not work, need robot and controller to see if i can print out this string when i press button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,9 @@ import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.shooter;
 
+
+
+
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
---
+++ src/main/java/frc/robot/test.java
@@ -0,0 +1,10 @@
+package frc.robot;
+public class test {
+    
+    public static void main(String[] args){
+        String testing = "try to use string for output in term";
+
+        System.out.println(testing);
+    }
+
+}
